14164e777b78de19b0265929892697a03dc9d711
footersaib <hamxa564927@gmail.com>
2022-02-21 12:44:39 -0800

    updated

--- app/Http/Middleware/Adminmiddleware.php
+++ app/Http/Middleware/Adminmiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Adminmiddleware
 {
@@ -16,6 +17,24 @@ class Adminmiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        if (Auth::check()) {
+            if(Auth::user()->role_as=='1'){ //admin=1
+
+                return $next($request);
+
+            }
+            else  {
+                return redirect('/home')->with('status','access denied');
+
+            }
+
+
+
+        }
+            else{
+                return redirect('/login')->with('status','please login first');
+
+            }
+
     }
 }
