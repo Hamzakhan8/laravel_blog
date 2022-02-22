d2e89e5e353fe3bc3ee6b2de4e1a53e7a254e768
footersaib <hamxa564927@gmail.com>
2022-02-22 04:44:27 -0800

    updated

---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.master')
+@section('title','category')
+
+
+@section('content')
+<div class="container-fluid px-4">
+    <h1 class="mt-4">category</h1>
+    <ol class="breadcrumb mb-4">
+        <li class="breadcrumb-item active">category</li>
+    </ol>
+
+    </div>
+    <div class="row">
+
+
+    </div>
+@endsection
