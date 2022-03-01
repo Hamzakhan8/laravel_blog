c09438a724ff6d7a33562ea63d0cfd2eec1ed7e9
footersaib <hamxa564927@gmail.com>
2022-02-28 22:48:41 -0800

    updated

--- app/Http/Controllers/Fronend/FrontEndController.php
+++ app/Http/Controllers/Fronend/FrontEndController.php
@@ -19,7 +19,7 @@ class FrontEndController extends Controller
 
         if($category){
 
-            $post=Post::where('category_id',$category->id)->where('status','0')->get();
+            $post=Post::where('category_id',$category->id)->where('status','0')->paginate(2);
             return view('frontend.post.index',compact('post','category'));
 
         }
--- resources/views/frontend/post/index.blade.php
+++ resources/views/frontend/post/index.blade.php
@@ -8,12 +8,32 @@
             <div class="row">
                 <div class="col-md-9">
 
-                        <div class="category-heading"></div>
+                        <div class="category-heading">
                         <h4>{{$category->name}}</h4>
+                    </div>
+                    @forelse ($post as $postitem)
+                    <div class="card card-shadow mt-4">
+                    <a href="{{url('tutorial/'.$category->slug.'/'.$postitem->slug)}}" class="text-decoration-none">
+                        <h2 class="post-heading">{{$postitem->name}}</h2>
+                    </a>
+                    <h6>Post on: {{$postitem->created_at->format('d-m-t')}}
+                        <span class="ms-3">Post by: {{$postitem->user->name}}</span>
+                    </h6>
 
-                        <div class="card card-shadow mt-4">
-                            <h2>hello</h2>
-                        </div>
+
+
+                    </div>
+
+
+                    @empty
+                    <div class="card card-shadow mt-4">
+                        <h2 class="">no Post Aailable </h2>
+                     </div>
+                    @endforelse
+
+                    <div class="your-paginate">
+                        {{$post->links()}}
+                    </div>
                 </div>
 
 
